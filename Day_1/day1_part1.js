b405d8a20272e5c840e082972d01b7bc40a5aa93
robbaer96 <baer.robert96@gmx.de>
2022-11-27 00:40:55 +0100

    update Frontend

--- Day_1/day1_part1.js
+++ Day_1/day1_part1.js
@@ -1,1 +1,8 @@
-console.log("Start");
+var items = [];
+function buildarray() {
+    boxvalue = document.getElementById('box').value;
+    items.push(boxvalue);  
+    items = items.toString().split(/\r?\n/);
+    console.log(items);
+    return false;
+  }
