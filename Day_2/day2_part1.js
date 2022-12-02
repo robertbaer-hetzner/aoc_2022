cac165e70340ecb113b482e20092bc898437e2fa
robbae <robert@baerco.de>
2022-12-02 20:03:43 +0100

    update code to be better readable

--- Day_2/day2_part1.js
+++ Day_2/day2_part1.js
@@ -1,37 +1,30 @@
-
 var items = [];
+var points = 0;
+
+function strategyGuide1(turn) {
+  if (turn[0] == "A" && turn[1] == "X"){points = points + 4; }
+  if (turn[0] == "A" && turn[1] == "Y"){points = points + 8; }
+  if (turn[0] == "A" && turn[1] == "Z"){points = points + 3; }
+  
+  if (turn[0] == "B" && turn[1] == "X"){points = points + 1; }
+  if (turn[0] == "B" && turn[1] == "Y"){points = points + 5; }
+  if (turn[0] == "B" && turn[1] == "Z"){points = points + 9; }
+
+  if (turn[0] == "C" && turn[1] == "X"){points = points + 7; }
+  if (turn[0] == "C" && turn[1] == "Y"){points = points + 2; }
+  if (turn[0] == "C" && turn[1] == "Z"){points = points + 6; }
+};
+
 function buildarray() {
     boxvalue = document.getElementById('box').value;
     items.push(boxvalue);  
     items = items.toString().split(/\r?\n/).map(function(e){ return e.split(" ");});
-    var points = 0;
     /*
-    A = Rock
-    B = Paper
-    C = Scissor
-
-    X = Rock 1
-    Y = Paper 2
-    Z = Scissor 3
-
-    Win = 6 P
-    Draw = 3 P 
-    Lose = 0 P
+    A = Rock , B = Paper , C = Scissor
+    X = Rock 1 , Y = Paper 2 , Z = Scissor 3
+    Win = 6 P , Draw = 3 P , Lose = 0 P
     */
-    for (var counter1 = 0; counter1 < items.length; counter1++) {
-      if (items[counter1][0] == "A" && items[counter1][1] == "X"){points = points + 4; }
-      if (items[counter1][0] == "A" && items[counter1][1] == "Y"){points = points + 8; }
-      if (items[counter1][0] == "A" && items[counter1][1] == "Z"){points = points + 3; }
-      
-      if (items[counter1][0] == "B" && items[counter1][1] == "X"){points = points + 1; }
-      if (items[counter1][0] == "B" && items[counter1][1] == "Y"){points = points + 5; }
-      if (items[counter1][0] == "B" && items[counter1][1] == "Z"){points = points + 9; }
-
-      if (items[counter1][0] == "C" && items[counter1][1] == "X"){points = points + 7; }
-      if (items[counter1][0] == "C" && items[counter1][1] == "Y"){points = points + 2; }
-      if (items[counter1][0] == "C" && items[counter1][1] == "Z"){points = points + 6; } 
-      console.log(points);
-    }  
+    items.map(strategyGuide1);
     document.getElementById("box2").innerHTML += "If everything goes exactly according to the strategy guide: " + points + "\n\n";
     return false;
   }
--- Day_2/day2_part2.js
+++ Day_2/day2_part2.js
@@ -1,39 +1,27 @@
-
-
 var items = [];
+var points2 = 0;
+
+function strategyGuide2(turn){
+  if (turn[1] == "X" && turn[0] == "A"){points2 = points2 + 3; }
+  if (turn[1] == "X" && turn[0] == "B"){points2 = points2 + 1; }
+  if (turn[1] == "X" && turn[0] == "C"){points2 = points2 + 2; }
+  
+  if (turn[1] == "Y" && turn[0] == "A"){points2 = points2 + 4; }
+  if (turn[1] == "Y" && turn[0] == "B"){points2 = points2 + 5; }
+  if (turn[1] == "Y" && turn[0] == "C"){points2 = points2 + 6; }
+
+  if (turn[1] == "Z" && turn[0] == "A"){points2 = points2 + 8; }
+  if (turn[1] == "Z" && turn[0] == "B"){points2 = points2 + 9; }
+  if (turn[1] == "Z" && turn[0] == "C"){points2 = points2 + 7; } 
+}
+
 function buildarray2() {
-    var points2 = 0;
     /*
-    A = Rock
-    B = Paper
-    C = Scissor
-
-    Rock 1
-    Paper 2
-    Scissor 3
-
-    X = Lose
-    Y = Draw
-    Z = Win
-
-    Win = 6 P
-    Draw = 3 P 
-    Lose = 0 P
-    */
-    for (var counter2 = 0; counter2 < items.length; counter2++) {
-      if (items[counter2][1] == "X" && items[counter2][0] == "A"){points2 = points2 + 3; }
-      if (items[counter2][1] == "X" && items[counter2][0] == "B"){points2 = points2 + 1; }
-      if (items[counter2][1] == "X" && items[counter2][0] == "C"){points2 = points2 + 2; }
-      
-      if (items[counter2][1] == "Y" && items[counter2][0] == "A"){points2 = points2 + 4; }
-      if (items[counter2][1] == "Y" && items[counter2][0] == "B"){points2 = points2 + 5; }
-      if (items[counter2][1] == "Y" && items[counter2][0] == "C"){points2 = points2 + 6; }
-
-      if (items[counter2][1] == "Z" && items[counter2][0] == "A"){points2 = points2 + 8; }
-      if (items[counter2][1] == "Z" && items[counter2][0] == "B"){points2 = points2 + 9; }
-      if (items[counter2][1] == "Z" && items[counter2][0] == "C"){points2 = points2 + 7; } 
-      console.log(points2 + " " + items[counter2][0] + " " + items[counter2][1]);
-    }  
+    A = Rock , B = Paper , C = Scissor
+    X = Rock 1 , Y = Paper 2 , Z = Scissor 3
+    Win = 6 P , Draw = 3 P , Lose = 0 P
+    */ 
+    items.map(strategyGuide2);
     document.getElementById("box2").innerHTML += "If everything goes exactly according to the second strategy guide: " + points2 + "\n\n";
     return false;
   }
