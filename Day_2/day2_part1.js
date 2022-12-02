ebab253192dbd878b0cd0e43fefecaa3c5602c76
robbaer96 <baer.robert96@gmx.de>
2022-12-02 06:45:29 +0100

    Finish Day2

--- Day_2/day2_part1.js
+++ Day_2/day2_part1.js
@@ -1,16 +1,37 @@
 
 var items = [];
-var sumarray = [];
 function buildarray() {
     boxvalue = document.getElementById('box').value;
     items.push(boxvalue);  
-    items = items.toString().split(/\r?\n/);
-    var caloriesSum = 0;
-    items.forEach(element => {
-      if (element == "") { sumarray.push(caloriesSum); caloriesSum = 0; }
-      else {caloriesSum = caloriesSum + parseInt(element);}
-    });
-    
-    document.getElementById("box2").innerHTML += "The Maximum of Caloriens from one elf is: " + Math.max.apply(Math, sumarray) + "\n\n";
+    items = items.toString().split(/\r?\n/).map(function(e){ return e.split(" ");});
+    var points = 0;
+    /*
+    A = Rock
+    B = Paper
+    C = Scissor
+
+    X = Rock 1
+    Y = Paper 2
+    Z = Scissor 3
+
+    Win = 6 P
+    Draw = 3 P 
+    Lose = 0 P
+    */
+    for (var counter1 = 0; counter1 < items.length; counter1++) {
+      if (items[counter1][0] == "A" && items[counter1][1] == "X"){points = points + 4; }
+      if (items[counter1][0] == "A" && items[counter1][1] == "Y"){points = points + 8; }
+      if (items[counter1][0] == "A" && items[counter1][1] == "Z"){points = points + 3; }
+      
+      if (items[counter1][0] == "B" && items[counter1][1] == "X"){points = points + 1; }
+      if (items[counter1][0] == "B" && items[counter1][1] == "Y"){points = points + 5; }
+      if (items[counter1][0] == "B" && items[counter1][1] == "Z"){points = points + 9; }
+
+      if (items[counter1][0] == "C" && items[counter1][1] == "X"){points = points + 7; }
+      if (items[counter1][0] == "C" && items[counter1][1] == "Y"){points = points + 2; }
+      if (items[counter1][0] == "C" && items[counter1][1] == "Z"){points = points + 6; } 
+      console.log(points);
+    }  
+    document.getElementById("box2").innerHTML += "If everything goes exactly according to the strategy guide: " + points + "\n\n";
     return false;
   }
--- Day_2/day2_part2.js
+++ Day_2/day2_part2.js
@@ -1,15 +1,39 @@
 
+
 var items = [];
-var sumarray = [];
-var topThree = 0;
 function buildarray2() {
+    var points2 = 0;
+    /*
+    A = Rock
+    B = Paper
+    C = Scissor
 
-    for(var i = 0; i < 3; i++){
-      const max = Math.max.apply(Math, sumarray);
-      topThree = topThree + max;
-      sumarray.splice(sumarray.indexOf(max),1);
-    }
+    Rock 1
+    Paper 2
+    Scissor 3
 
-    document.getElementById("box2").innerHTML += "The Maximum of Caloriens from the top three elfs are: " + topThree;
+    X = Lose
+    Y = Draw
+    Z = Win
+
+    Win = 6 P
+    Draw = 3 P 
+    Lose = 0 P
+    */
+    for (var counter2 = 0; counter2 < items.length; counter2++) {
+      if (items[counter2][1] == "X" && items[counter2][0] == "A"){points2 = points2 + 3; }
+      if (items[counter2][1] == "X" && items[counter2][0] == "B"){points2 = points2 + 1; }
+      if (items[counter2][1] == "X" && items[counter2][0] == "C"){points2 = points2 + 2; }
+      
+      if (items[counter2][1] == "Y" && items[counter2][0] == "A"){points2 = points2 + 4; }
+      if (items[counter2][1] == "Y" && items[counter2][0] == "B"){points2 = points2 + 5; }
+      if (items[counter2][1] == "Y" && items[counter2][0] == "C"){points2 = points2 + 6; }
+
+      if (items[counter2][1] == "Z" && items[counter2][0] == "A"){points2 = points2 + 8; }
+      if (items[counter2][1] == "Z" && items[counter2][0] == "B"){points2 = points2 + 9; }
+      if (items[counter2][1] == "Z" && items[counter2][0] == "C"){points2 = points2 + 7; } 
+      console.log(points2 + " " + items[counter2][0] + " " + items[counter2][1]);
+    }  
+    document.getElementById("box2").innerHTML += "If everything goes exactly according to the second strategy guide: " + points2 + "\n\n";
     return false;
   }
