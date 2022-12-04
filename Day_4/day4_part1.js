792992ff9281033c1831ca943c1d46e5f5597b2e
robbaer96 <baer.robert96@gmx.de>
2022-12-04 08:35:19 +0100

    solve Day4

--- Day_4/day4_part1.js
+++ Day_4/day4_part1.js
@@ -1,3 +1,4 @@
+let overlaps = 0; 
 function buildarray(items){
     items = [];
     boxvalue = document.getElementById('box').value;
@@ -6,9 +7,24 @@ function buildarray(items){
     return items;
 }
 
+function range(size, startAt = 0) {
+    return [...Array(size).keys()].map(i => i + startAt);
+}
+
+function checkFullOverlapping(sections){
+  sections = sections.split(',');
+  let elf1 = sections[0].split('-');
+  let elf2 = sections[1].split('-');
+  elf1 = range((parseInt(elf1[1]) - parseInt(elf1[0]) + 1),parseInt(elf1[0]));
+  elf2 = range((parseInt(elf2[1]) - parseInt(elf2[0]) + 1),parseInt(elf2[0]));
+  if (elf1.every(element => {return elf2.includes(element);}) || elf2.every(element => {return elf1.includes(element);})) {overlaps++;}
+
+}
+
 function part1() {
     
-    let arrayPart1 = buildarray(); 
-    document.getElementById("box2").innerHTML += "Result Part 1 "+ "\n\n";
+    let arrayPart1 = buildarray();
+    arrayPart1.map(checkFullOverlapping); 
+    document.getElementById("box2").innerHTML += "Result Part 1:  " + overlaps + "\n\n";
     return false;
   }
--- Day_4/day4_part2.js
+++ Day_4/day4_part2.js
@@ -1,6 +1,16 @@
+function checkAtAllOverlapping(sections){
+  sections = sections.split(',');
+  let elf1 = sections[0].split('-');
+  let elf2 = sections[1].split('-');
+  elf1 = range((parseInt(elf1[1]) - parseInt(elf1[0]) + 1),parseInt(elf1[0]));
+  elf2 = range((parseInt(elf2[1]) - parseInt(elf2[0]) + 1),parseInt(elf2[0]));
+  if (elf1.includes(elf2[0]) || elf1.includes(elf2[1]) || elf2.includes(elf1[0]) || elf2.includes(elf1[0])) {overlaps++;}
+}
 
 function part2() {
+  overlaps = 0;
   let arrayPart2 = buildarray(); 
-  document.getElementById("box2").innerHTML += "Result Part 2 "+ "\n\n";
+  arrayPart2.map(checkAtAllOverlapping);
+  document.getElementById("box2").innerHTML += "Result Part 2 " + overlaps + "\n\n";
   return false;
   }
