de4455422768bd50b7eee5afd818228aa12e2a74
robbaer96 <baer.robert96@gmx.de>
2022-12-06 21:37:14 +0100

    prepare day7

---
+++ Day_7/day7_part1.js
@@ -0,0 +1,16 @@
+let overlaps = 0; 
+function buildarray(items){
+    items = [];
+    boxvalue = document.getElementById('box').value;
+    items.push(boxvalue);  
+    items = items.toString().split('');
+    return items;
+}
+
+function part1() {
+    
+    let arrayPart1 = buildarray();
+
+    document.getElementById("box2").innerHTML += "Result Part 1:  " + "\n\n";
+    return false;
+  }
