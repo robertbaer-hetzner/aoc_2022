58749a201cba74d914636a5309fbf891e5eeb1ec
robbaer96 <baer.robert96@gmx.de>
2022-12-06 06:31:55 +0100

    update

--- Day_6/day6_part1.js
+++ Day_6/day6_part1.js
@@ -8,17 +8,11 @@ function buildarray(items){
 }
 
 function uniqueCharacters(str)
-{
-     
-    // If at any time we encounter 2
-    // same characters, return false
+{ 
     for(let i = 0; i < str.length; i++)
         for(let j = i + 1; j < str.length; j++)
             if (str[i] == str[j])
                 return false;
- 
-    // If no duplicate characters
-    // encountered, return true
     return true;
 }
 
