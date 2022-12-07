d64180ad3b8c202e454028c523c458780cba4d5b
robbaer96 <baer.robert96@gmx.de>
2022-12-07 21:35:00 +0100

    .

--- Day_7/day7_part1.js
+++ Day_7/day7_part1.js
@@ -2,14 +2,42 @@ function buildarray(items){
     items = [];
     boxvalue = document.getElementById('box').value;
     items.push(boxvalue);  
-    items = items.toString().split('');
+    items = items.toString().split(/\r?\n/);
+    items.shift();
     return items;
 }
 
-function part1() {
-    
-    let arrayPart1 = buildarray();
+function mapDirectory(lines) {
+    let subDirectory = [];
+    let depth = 0;
 
-    document.getElementById("box2").innerHTML += "Result Part 1:  " + "\n\n";
+    lines.some(line => {
+      if (line.substring(0, 4) == '$ cd' && line.substring(0, 7) !== '$ cd ..') depth++;
+      if (line.substring(0, 7) == '$ cd ..') {
+        if (depth == 0) return true;
+        depth--;
+      }
+      subDirectory.push(line);
+    });
+  
+    const size = subDirectory.map(item => parseInt(item.split(' ')[0])).filter(Number).reduce((a, c) => a + c);
+    return size; 
+  }
+
+function part1() {
+    let directories = []; 
+    let arraypart1 = buildarray();
+    
+    arraypart1.forEach((line, index) => {
+        if (line.substring(0, 4) === '$ ls') {
+          const remainingLines = arraypart1.slice(index+1);
+          const dirSize = mapDirectory(remainingLines);
+          directories.push(dirSize);
+        }
+      });
+      
+    let solve = directories.filter(size => size <= 100000).reduce((a, c) => a + c);
+
+    document.getElementById("box2").innerHTML += "Result Part 1:  " + solve + "\n\n";
     return false;
   }
--- Day_7/day7_part2.js
+++ Day_7/day7_part2.js
@@ -1,7 +1,23 @@
 
 function part2() {
   let arrayPart2 = buildarray();
+  let directories = []; 
 
-  document.getElementById("box2").innerHTML += "Result Part 2: " + "\n\n";
+  arrayPart2.forEach((line, index) => {
+    if (line.substring(0, 4) == '$ ls') {
+      const remainingLines = arrayPart2.slice(index+1);
+      const dirSize = mapDirectory(remainingLines);
+      directories.push(dirSize);
+    }
+  });
+
+  let diskSize = 70000000;
+  let requiredSpace = 30000000;
+  
+  let extraSpaceNeeded = requiredSpace - (diskSize - directories[0]);
+  let possibleDirs = directories.filter(size => size >= extraSpaceNeeded);
+  let solve = Math.min(...possibleDirs);
+
+  document.getElementById("box2").innerHTML += "Result Part 2: " + solve + "\n\n";
   return false;
   }
